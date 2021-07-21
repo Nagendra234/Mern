bb0a60760ef5b2c34ce5fbc2837b3f246d7bdd12
Nagendra <naginagendra234@gmail.com>
2021-07-21 07:45:03 +0530

    Homepage added in app.js

--- frontend/interface/src/App.js
+++ frontend/interface/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Register from './components/Register';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       <Register />
     </div>
   );
