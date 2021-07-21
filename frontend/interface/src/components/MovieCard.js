201e90193f5edd3d7fbb324c318c6508bb688b4b
Nagendra <naginagendra234@gmail.com>
2021-07-21 13:02:50 +0530

    Movie card page added and images added

---
+++ frontend/interface/src/components/MovieCard.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import crd1 from "../images/Card1.jpg"
+import crd2 from "../images/Card2.jpg"
+import crd3 from "../images/Card3.jpg"
+class MovieCard extends Component {
+    render() {
+        return (
+            <div>
+                <div className="card" >
+                    <a href="/pushpa">
+                        <img src={crd1} className="card-img" alt="..." />
+                    </a>
+                    <div className="card-body">
+                        <h5 className="card-title">Pushpa</h5>
+                    </div>
+                </div>
+                <div className="card" >
+                    <a href="/kgf2">
+                        <img src={crd2} className="card-img" alt="..." />
+                    </a>
+                    <div className="card-body">
+                        <h5 className="card-title">KGF 2</h5>
+                    </div>
+                </div>
+                <div className="card" >
+                    <a href="/radhesyam">
+                        <img src={crd3} className="card-img" alt="..." />
+                    </a>
+                    <div className="card-body">
+                        <h5 className="card-title">Radhe Syam</h5>
+                    </div>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+
+export default MovieCard
