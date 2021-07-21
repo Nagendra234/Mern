91acd96f76d7cf8db06e53eec48a23603cde4a87
Nagendra <naginagendra234@gmail.com>
2021-07-21 13:43:42 +0530

    added css page inside Component

--- frontend/interface/src/components/Kgf.js
+++ frontend/interface/src/components/Kgf.js
@@ -11,26 +11,28 @@ class Kgf extends Component {
                     <h2>Details Of the Movie</h2>
                     <p><strong>Pushpa</strong> is Upcoming Movie. Futured Stars are hero as <strong>Allu Arjun</strong> and Heroine as <strong>Rashmika</strong> and this movie Directed By<strong> Sukumar</strong> </p>
                     <table>
-                        <tr>
-                            <td>Produced By</td>
-                            <td>Naveen and N. Ravi Shankar</td>
-                        </tr>
-                        <tr>
-                            <td>Starring</td>
-                            <td>Allu Arjun Fahadh Faasil Rashmika Mandanna</td>
-                        </tr>
-                        <tr>
-                            <td>Cinematography	</td>
-                            <td>Mirosław Kuba Brożek</td>
-                        </tr>
-                        <tr>
-                            <td>Edited by	</td>
-                            <td>Karthika Srinivas</td>
-                        </tr>
-                        <tr>
-                            <td>Story & Direction By </td>
-                            <td>Sukumar</td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>Produced By</td>
+                                <td>Naveen and N. Ravi Shankar</td>
+                            </tr>
+                            <tr>
+                                <td>Starring</td>
+                                <td>Allu Arjun Fahadh Faasil Rashmika Mandanna</td>
+                            </tr>
+                            <tr>
+                                <td>Cinematography	</td>
+                                <td>Mirosław Kuba Brożek</td>
+                            </tr>
+                            <tr>
+                                <td>Edited by	</td>
+                                <td>Karthika Srinivas</td>
+                            </tr>
+                            <tr>
+                                <td>Story & Direction By </td>
+                                <td>Sukumar</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
 
