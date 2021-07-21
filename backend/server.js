b5e5b667e59bf29604d1addf68e71dc512ad5888
Nagendra <naginagendra234@gmail.com>
2021-07-21 19:38:28 +0530

    deleled files

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const App = express()
-
-
-App.listen(4500, function (req, res) {
-    console.log("server running on 4000")
-})
--- frontend/interface/src/components/Register.js
+++ frontend/interface/src/components/Register.js
@@ -7,25 +7,25 @@ class Register extends Component {
                 <form>
                     <div class="form-group">
                         <label for="fullName" className="label-name"> Full Name</label>
-                        <input type="text" class="form-control" placeholder="Enter Your Full Name" />
+                        <input type="text" className="form-control" placeholder="Enter Your Full Name" />
                     </div>
                     <div class="form-group">
                         <label for="email" className="label-name">Email address</label>
-                        <input type="email" class="form-control" placeholder="Enter Your Full Name" />
+                        <input type="email" className="form-control" placeholder="Enter Your Full Name" />
                     </div>
                     <div class="form-group">
                         <label for="mobileNumber" className="label-name">Mobile Number</label>
-                        <input type="text" class="form-control" placeholder="Enter Your Full Name" />
+                        <input type="text" className="form-control" placeholder="Enter Your Full Name" />
                     </div>
                     <div class="form-group">
                         <label for="password" className="label-name">Password</label>
-                        <input type="password" class="form-control" placeholder="Enter Your Full Name" />
+                        <input type="password" className="form-control" placeholder="Enter Your Full Name" />
                     </div>
                     <div class="form-group">
                         <label for="confirmPass">Confirm Password</label>
-                        <input type="password" class="form-control" placeholder="Enter Your Full Name" />
+                        <input type="password" className="form-control" placeholder="Enter Your Full Name" />
                     </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
+                    <button type="submit" className="btn btn-primary">Register</button>
                 </form>
             </div>
         )
