6ce8d9c7a6c8f3f0a0175f68cc62eeb7412fd956
Nagendra <naginagendra234@gmail.com>
2021-07-21 20:10:24 +0530

    Schema Added

---
+++ backend/server/Models/userSchema.js
@@ -0,0 +1,25 @@
+const mongooose = require('mongoose');
+
+const userSchema = new mongooose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+})
+
+const User = mongooose.model('USER', userSchema);
+
+module.exports = User;
