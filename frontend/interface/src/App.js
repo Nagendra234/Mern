6b1fd51e69b85f0982339ffb0a6b5cd7f6835318
Nagendra <naginagendra234@gmail.com>
2021-07-21 13:06:26 +0530

    Moviecard added in App.js

--- frontend/interface/src/App.js
+++ frontend/interface/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Register from './components/Register';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <MovieCard />
       <Register />
     </div>
   );
