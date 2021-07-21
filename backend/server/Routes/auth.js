003dc644c220fc1cb804a01f8de426f78a99c011
Nagendra <naginagendra234@gmail.com>
2021-07-21 21:32:13 +0530

    backend created

--- backend/server/Routes/auth.js
+++ backend/server/Routes/auth.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../Models/userSchema")
 
 router.get('/', (req, res) => {
     res.send(`Hello world from the server rotuer js`);
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({ message: req.body });
-    // res.send("mera register page");
+    registeredUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password
+    })
+    registeredUser.save()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => {
+            res.json(error)
+        })
 });
 
 module.exports = router;
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,10 +1,30 @@
-const express = require("express")
-const App = express()
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-App.get('/', function (req, res) {
-    res.send("<h1>Hello world</h1>")
-})
+const DB = "mongodb+srv://Muppalla:Nagendra@cluster0.ulgxz.mongodb.net/MernStack?retryWrites=true&w=majority"
 
-App.listen(4000, function (req, res) {
-    console.log("server is running")
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log(`connnection successful`);
+}).catch((err) => console.log(`no connection`));
+
+// const User = require('./model/userSchema');
+
+app.use(express.json());
+
+// we link the router files to make our route easy 
+app.use(require('./Routes/auth'));
+
+
+app.get('/register', (req, res) => {
+    res.send(`Hello Registration world from the server`);
+});
+
+app.listen(4000, () => {
+    console.log(`server is runnig at port no 4000`);
 })
